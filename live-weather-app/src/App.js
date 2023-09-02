a1898e8984fe755c357855bb9e67578a986bd801
Srideep Kar <srideepkar@gmail.com>
2023-09-03 00:54:35 +0530

    Changes

--- live-weather-app/src/App.js
+++ live-weather-app/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import WeatherCard from './WeatherCard';
+import { useState } from 'react';
+
 
 function App() {
+  const [createCard, setCreateCard] = useState();
+
+  const addCard = () => {
+    setCreateCard(<WeatherCard className = 'cards'/>);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Live weather tracking</a>
+          </div>
+          <ul className="nav navbar-nav">
+            <li className="active"><a href="#">Dashboard</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className='cardsTable'>
+        <WeatherCard className = 'cards'/>
+        <WeatherCard className = 'cards'/>
+        <WeatherCard className = 'cards'/>
+        <WeatherCard className = 'cards'/>
+        <button className = 'cards' onClick={addCard}>+</button>        
+      </div>
+    </>
   );
 }
 
